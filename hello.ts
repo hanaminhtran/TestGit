8008a2ab504dd5548092f80714b6b396fd333fae
hanaminhtran <vnpalm@gmail.com>
2021-09-06 02:08:01 +0700

    add field Ower to Car class

--- hello.ts
+++ hello.ts
@@ -1,21 +1,23 @@
 
-
 class Car{
     Brand: string;
     Year: number;
+    Ower : string;
 
-    constructor(brand: string, year: number){
+    constructor(brand: string, year: number, ower: string){
         this.Brand = brand;
         this.Year = year;
+        this.Ower = ower;
     }
     Display(){
         console.log(this.Brand);
         console.log(this.Year);
+        console.log(this.Ower);
     }
 }
 //change hello.ts 4
 
 
-let mes = new Car("Mescedess",2015);
+let mes = new Car("Mescedess",2015, "Minh");
 mes.Display();
 
